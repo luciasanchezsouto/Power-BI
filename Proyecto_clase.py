656974d7800cab66407fe7d7db846ffaa5f30153
Lucía Sánchez Souto <sanchezsoutolucia@gmail.com>
2024-07-09 20:11:57 +0200

    Manufacturer-Analysis.py
    Power BI example

--- Proyecto_clase.py
+++ Proyecto_clase.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.title('Manufacturer Analysis in PowerBI')
-power_bi_url = "https://app.powerbi.com/links/yRJxfEl0_C?ctid=8aebddb6-3418-43a1-a255-b964186ecc64&pbi_source=linkShare&bookmarkGuid=cc6817e0-6ba2-49fa-96f8-02a552a2565f"
+power_bi_url = "https://app.powerbi.com/reportEmbed?reportId=a83d22ca-f8af-4671-8d23-2bdb2c401324&autoAuth=true&ctid=8aebddb6-3418-43a1-a255-b964186ecc64"
 st.markdown(
        f"""
        <div style="display: flex; justify-content: center;">
